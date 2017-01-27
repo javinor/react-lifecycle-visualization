b91fcc1a19c85917a8a0991f4b95a1a2cd7e04d9
javinor <javinor@gmail.com>
2017-01-27 17:02:07 +0200

    add colorful backgrounds

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ const ReactDOM = require('react-dom')
 const start = Date.now()
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
 }
 
 class LifeCycleLogger extends React.Component {
@@ -70,8 +70,4 @@ class App extends React.Component {
   }
 }
 
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App/>, document.getElementById('app'))
